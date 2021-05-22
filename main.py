431f4327d9e0292a2365db61c56997e34cd583ca
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-22 18:26:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import uuid
+import shutil
+import logging
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from creds import Credentials
+from telegraph import upload_file
+
+logging.basicConfig(level=logging.INFO)
+
+
+TGraph = Client(
+    "Image upload bot",
+    bot_token=Credentials.BOT_TOKEN,
+    api_id=Credentials.API_ID,
+    api_hash=Credentials.API_HASH,
+)
+
+
+@TGraph.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text(
+        text=f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @TGBotSzK",
+        disable_web_page_preview=True
+    )
+
+
+@TGraph.on_message(filters.photo)
+async def getimage(client, message):
+    tmp = os.path.join("downloads", str(message.chat.id))
+    if not os.path.isdir(tmp):
+        os.makedirs(tmp)
+    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
+    dwn = await message.reply_text("Downloading to my server...", True)
+    img_path = await client.download_media(message=message, file_name=img_path)
+    await dwn.edit_text("Uploading as telegra.ph link...")
+    try:
+        response = upload_file(img_path)
+    except Exception as error:
+        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        return
+    await dwn.edit_text(
+        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>,\n\n<b>Join:-</b> @TGBotSzK",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔗 Open Link", url=f"https://telegra.ph{response[0]}"), InlineKeyboardButton(text="Share Link 👥", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")]])
+    )
+    shutil.rmtree(tmp, ignore_errors=True)
+
+
+TGraph.run()
