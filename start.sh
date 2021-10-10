8ffd74a6b8ee9c05d36ad0d73ddf6d062585ab72
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-10 11:16:41 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ZauteKm/Image-Upload-Bot.git /Image-Upload-Bot
+cd /Image-Upload-Bot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
